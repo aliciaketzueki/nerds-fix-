7a2060b31232367c5c76a89909ea42475b76a054
Aliciaketzueki <aliciaketzueki@gmail.com>
2018-05-21 20:35:02 +0300

    Исправлены CRLF на LF, настроены editorconfig и stylelint, исправлены ошиб ки editorconfig и stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,24 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-var autoprefixer = require("gulp-autoprefixer");
+var autoprefixer = require("autoprefixer");
 var browserSync = require("browser-sync").create();
 
 gulp.task("style", function() {
   gulp.src("source/less/style.less")
-		.pipe(plumber())
-		.pipe(less())
-		.pipe(postcss([
-			autoprefixer()
-		]))
-		.pipe(gulp.dest("source/css"))
-		.pipe(browserSync.stream());
+  .pipe(plumber())
+  .pipe(less())
+  .pipe(postcss([
+    autoprefixer()
+  ]))
+  .pipe(gulp.dest("source/css"))
+  .pipe(browserSync.stream());
 });
 
 gulp.task("serve", ["style"], function() {
-	browserSync.init({
-		server: "source/",
-
-	});
-	gulp.watch("source/less/**/*.less", ["style"]);
-	gulp.watch("source/*.html").on("change", browserSync.reload);
+  browserSync.init({
+    server: "source/",
+  });
+  gulp.watch("source/less/**/*.less", ["style"]);
+  gulp.watch("source/*.html").on("change", browserSync.reload);
 });
