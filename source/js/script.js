b89c98210cfe0e041a433f4c0edbad5d094ceb70
Aliciaketzueki <aliciaketzueki@gmail.com>
2018-05-22 05:57:12 +0300

    Частичная верстка блоков about-us и projects

--- source/js/script.js
+++ source/js/script.js
@@ -1,6 +1,6 @@
 var slides = document.querySelectorAll(".slider__list .slider__item");
 var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 10000);
+var slideInterval = setInterval(nextSlide, 5000);
 
 function nextSlide() {
   slides[currentSlide].className = "slider__item";
