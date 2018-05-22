f27d670999b802db9bbaa9abc4fede293a4790bc
Aliciaketzueki <aliciaketzueki@gmail.com>
2018-05-22 15:43:15 +0300

    Верстка футера

--- source/js/script.js
+++ source/js/script.js
@@ -7,3 +7,18 @@ function nextSlide() {
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].className = "slider__item slider__item--show";
 }
+
+function initMap() {
+  var uluru = { lat: 59.93912426, lng: 30.32130726 };
+  var map = new google.maps.Map(document.querySelector(".map__container"), {
+    zoom: 17,
+    center: uluru
+  });
+  var image = "img/map-marker.png";
+  var markercenter = { lat: 59.93876956, lng: 30.32377489 };
+  var marker = new google.maps.Marker({
+    position: markercenter,
+    map: map,
+    icon: image
+  });
+}
